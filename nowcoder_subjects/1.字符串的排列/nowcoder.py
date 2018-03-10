6da5e75c36582da8983d2ddf1875a6d97b59c805
s969966195 <969966195@qq.com>
2018-03-10 16:18:37 +0800

    .

--- nowcoder_subjects/1.字符串的排列/nowcoder.py
+++ nowcoder_subjects/1.字符串的排列/nowcoder.py
@@ -8,7 +8,24 @@ class Solution:
         if not ss:
             return []
 
-        return sorted(list(set(map(''.join, itertools.permutations(ss)))))
+        # return sorted(list(set(map(''.join, itertools.permutations(ss)))))
+        self.vis = [0 for i in range(len(ss))]
+        self.res = []
+        self.temp = ""
+        self.DFS(0, ss)
+        return sorted([i for i in set(self.res)])
+
+    def DFS(self, su, ss):
+        if su == len(ss):
+            self.res.append(self.temp)
+            return
+        for i in range(len(ss)):
+            if self.vis[i] == 0:
+                self.vis[i] = 1
+                self.temp += ss[i]
+                self.DFS(su+1, ss)
+                self.temp = self.temp[:-1]
+                self.vis[i] = 0
 
 
 s = Solution()
