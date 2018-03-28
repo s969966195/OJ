da862608365eb922af71a966b18295d7ee381df3
s969966195 <969966195@qq.com>
2018-03-28 22:34:04 +0800

    .

--- nowcoder_subjects/1.数组中重复的数字/nowcoder.py
+++ nowcoder_subjects/1.数组中重复的数字/nowcoder.py
@@ -4,11 +4,24 @@ class Solution:
     # 函数返回True/False
     def duplicate(self, numbers, duplication):
         # write code here
+        '''
         for i in range(len(numbers)):
             if numbers.count(numbers[i]) > 1:
                 duplication[0] = numbers[i]
                 return True
 
+        return False
+        '''
+        # 因为题目里写了数组里数字在0 ~ n-1 之间，所以可以利用现有数组设置标志，当一个数字被访问过后，可以设置对应位上的数 + n，之后再遇到相同的数时，会发现对应位上的数已经大于等于n了，那么直接返回这个数即可。
+        length = len(numbers)
+        for i in range(length):
+            index = numbers[i]
+            if index >= length:
+                index -= length
+            if numbers[index] >= length:
+                duplication[0] = index
+                return True
+            numbers[index] = numbers[index] + length
         return False
 
 
